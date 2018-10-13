b75d2d22ce036ae3af602df9ad4c587d1e827f7f
gsicuto <gabrielsicuto@gmail.com>
2018-10-13 16:03:07 -0300

    Inclusão função Backward
    inclusão de nova função e ajuste no posicionamento da função log.increment

--- MarsRover/javascripts/app.js
+++ MarsRover/javascripts/app.js
@@ -18,54 +18,39 @@ posX: 5,
 posY: 5,
 travelLog: new Array()};
 
-// ======================
-
-/*function teste(rover){
- rover.travelLog[0]="teste 01";
- rover.travelLog[rover.travelLog.length]="teste 02";
- console.log(rover.travelLog[0]);
- console.log(rover.travelLog[1]);
-}*/
-
 function incrementLog(rover){
-
   rover.travelLog[rover.travelLog.length] = "O Rover passou por: x: "+rover.posX+" y: "+rover.posY +" direção: "+rover.direction;
-
 }
 
 function turnLeft(rover){
-
   console.log("turnLeft was called!");
-
   switch (rover.direction){
     case "N":
      rover.direction = "W";
      console.log("Rover is facing West!");
-     
      break;
+
     case "W":
      rover.direction = "S";
      console.log("Rover is facing South!");
-     
      break;
+
     case "S":
      rover.direction = "E";
      console.log("Rover is facing East!");
-     
      break;
+
     case "E":
      rover.direction = "N";
      console.log("Rover is facing North!");
-     
      break;
   }
+  incrementLog(rover);
   console.log("Rover is in direction:" + rover.direction);
 }
 
 function turnRight(rover){
-  
   console.log("turnRight was called!");
-
   switch (rover.direction){
     case "N":
      rover.direction = "E";
@@ -88,6 +73,7 @@ function turnRight(rover){
      
      break;
   }
+  incrementLog(rover);
   console.log("Rover is in direction:" + rover.direction);
 }
 
@@ -112,44 +98,62 @@ function moveForward(rover){
       
       break;
   }
+  incrementLog(rover);
   console.log("A nova Posição é: x: "+rover.posX+" y: "+rover.posY);
 }
 
+function moveBackward(rover){
+  console.log("moveBackward was called")
+
+  switch (rover.direction){
+    case "N":
+     rover.posY = rover.posY+1;
+     break;
+
+    case "W":
+     rover.posX = rover.posX+1;
+     break;
+
+    case "S":
+     rover.posY = rover.posY-1;
+     break;
+
+    case "E":
+      rover.posX = rover.posX -1;
+      break;
+  }
+  incrementLog(rover);
+  console.log("A nova Posição é: x: "+rover.posX+" y: "+rover.posY);
+}
 
 function moveCommand (rover, task){
-
   var x = task;
-
-  
   for(var i = 0; i < x.length; i++){
 
-    if(x[i]!="f" && x[i]!= "l" && x[i]!="r"){
+    if(x[i]!="f" && x[i]!= "l" && x[i]!="r" && x[i]!="b"){
       console.log("Comando "+x[i]+" inválido!");
     }
 
     switch(x[i]){
       case "f":
       moveForward(rover);
-      incrementLog(rover);
       break;
+
       case "l":
       turnLeft(rover);
-      incrementLog(rover);
       break;
+
       case "r":
       turnRight(rover);
-      incrementLog(rover);
       break;
-    }
-   
+
+      case "b":
+      moveBackward(rover);
+      break;
+    } 
   }
-  
- 
   for(var i = 0; i< rover.travelLog.length; i++){
   
-    console.log(rover.travelLog[i]);
-  
+    console.log(rover.travelLog[i]); 
   }
-
-
 }
